bd8d444c42578af047618da04817e280dc8699be
Josh Baird <jjb.pub01@gmail.com>
2016-09-22 18:00:20 -0400

    Rename to remove underscores

--- source/edu/oakland/homework/hw01/SpecialCharacter.java
+++ source/edu/oakland/homework/hw01/SpecialCharacter.java
@@ -1,5 +1,5 @@
 package edu.oakland.homework.hw01;
-public class Special_Character{
+public class SpecialCharacter{
 	public static void main(String[] args){
 		System.out.println("Shaq is 7'1");
 		System.out.println("The string \"\" is an empty message.");

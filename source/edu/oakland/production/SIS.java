60747c45b5b2a6110c1d790bf33059316578dbe6
Josh Baird <jjb.pub01@gmail.com>
2016-09-26 14:23:54 -0400

    Add comments

--- source/edu/oakland/production/SIS.java
+++ source/edu/oakland/production/SIS.java
@@ -7,19 +7,37 @@ public class SIS {
 	private Student[] students;
 	private int numStudents = 0;
 
+	/**
+	 * Create a new SIS with the default capacity of 5 students.
+	 */
 	public SIS() {
 		students = new Student[5];
 	}
 
+	/**
+	 * Create a new SIS with the specified capacity of students.
+	 *
+	 * @param maximumStudents The maximum number of students that will be stored.
+	 */
 	public SIS(int maximumStudents) {
 		students = new Student[maximumStudents];
 	}
 
+	/**
+	 * Add a student to the SIS.
+	 *
+	 * @param student The {@link Student} to add.
+	 */
 	public void addStudent(Student student) {
 		this.students[numStudents] = student;
 		this.numStudents++;
 	}
 
+	/**
+	 * Retrieve a student from the SIS.
+	 *
+	 * @param GID The student's Grizzly ID.
+	 */
 	public Student getStudent(int GID) {
 		for (Student s : students) {
 			if (s.getGrizzlyID() == GID) {

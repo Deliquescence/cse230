eca372840fe3e2f15edc026447645e34e6351bc9
Josh Baird <jjb.pub01@gmail.com>
2016-11-16 11:12:42 -0500

    Add Launcher

---
+++ source/edu/oakland/production/Launcher.java
@@ -0,0 +1,24 @@
+package edu.oakland.production;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		JFrame gui = new SISGUI();
+
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		int width = screenSize.width;
+		int height = screenSize.height;
+
+		gui.setLocation(width / 4, height / 4);
+
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setTitle("SIS User Interface");
+		gui.setSize(500, 500);
+
+		gui.setVisible(true);
+	}
+}

adada73bfab80a43fea7f40a24e4043f5680b392
Josh Baird <jjb.pub01@gmail.com>
2016-11-21 10:44:43 -0500

    Add header labels to builder

--- source/edu/oakland/helper/LabelBuilder.java
+++ source/edu/oakland/helper/LabelBuilder.java
@@ -7,12 +7,14 @@ import javax.swing.*;
 
 public class LabelBuilder {
 
-
+	private JLabel inputHeader;
 	private JLabel inputGrizzlyID;
 	private JLabel inputName;
 	private JLabel inputStatus;
 	private JLabel inputProgress;
 	private JLabel inputMajor;
+
+	private JLabel outputHeader;
 	private JLabel outputGrizzlyID;
 	private JLabel outputName;
 	private JLabel outputStatus;
@@ -20,16 +22,28 @@ public class LabelBuilder {
 	private JLabel outputMajor;
 
 	public LabelBuilder() {
+		inputHeader = new JLabel("Data Input");
 		inputGrizzlyID = new JLabel("Grizzly ID");
 		inputName = new JLabel("Name");
 		inputStatus = new JLabel("Status (UnderGrad/Grad)");
 		inputProgress = new JLabel("Sat Progress for Degree");
 		inputMajor = new JLabel("Major");
+
+		outputHeader = new JLabel("Data Output");
 		outputGrizzlyID = new JLabel("Grizzly ID");
 		outputName = new JLabel("Name");
 		outputStatus = new JLabel("Status (UnderGrad/Grad)");
 		outputProgress = new JLabel("Sat Progress for Degree");
 		outputMajor = new JLabel("Major");
+
+		inputHeader.setFont(new Font(inputHeader.getFont().getName(), Font.BOLD, 16));
+		inputHeader.setHorizontalAlignment(JLabel.CENTER);
+		outputHeader.setFont(new Font(outputHeader.getFont().getName(), Font.BOLD, 16));
+		outputHeader.setHorizontalAlignment(JLabel.CENTER);
+	}
+
+	public JLabel getInputHeader() {
+		return inputHeader;
 	}
 
 	public JLabel getInputGrizzlyID() {
@@ -50,6 +64,11 @@ public class LabelBuilder {
 
 	public JLabel getInputProgress() {
 		return inputProgress;
+	}
+
+
+	public JLabel getOutputHeader() {
+		return outputHeader;
 	}
 
 	public JLabel getOutputGrizzlyID() {
